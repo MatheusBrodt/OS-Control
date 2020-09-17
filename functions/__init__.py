42c635f0faa8284d45a3e03895d9c3a6670dc133
Matheus Brodt <matheus_brodt@outlook.com>
2020-09-17 01:00:52 -0300

    Salvando no banco de dados
    Apenas salvando no banco de dados, proximo passo verificação antes de commitar no MySql

--- functions/__init__.py
+++ functions/__init__.py
@@ -1,7 +1,7 @@
 def inteiro(txt):
     """
     Compare if the number is an integer.
-    :param txt: Data input
+    :param txt: Data input.
     :return: If true return number.
     """
     opc = 0
@@ -16,6 +16,29 @@ def inteiro(txt):
                 return var_2
         except:
             print(f'\033[33mErro ao reconhecer um número inteiro!\033[m')
+
+def integer_with_bar(txt):
+    """
+    Compare if the number have bar.
+    :param txt: Data input.
+    :return: If true return number.
+    """
+    opc = 0
+    while opc == 0:
+        try:
+            var = str(input(txt))
+            if var.isalpha():
+                print(f'\033[33m"{var}"\033[m Opção Inválida!')
+            elif '/' in var:
+                var_2 = str(var)
+                opc = 1
+                return var_2
+            else:
+                var_2 = str(var)
+                opc = 1
+                return var_2
+        except:
+            print(f'\033[31mErro ao reconhecer um número de sequência!\033[m')
 
 
 def check_store(number):
@@ -35,3 +58,27 @@ def check_store(number):
         except:
             print(f'\033[31mErro ao digitar a filial!\033[m')
             continue
+
+def comparation(num):
+    if num == 1432:
+        return 'matriz'
+    elif num == 2064:
+        return 'centro_sao_leo'
+    elif num == 2007:
+        return 'shopping'
+    elif num == 1518:
+        return 'feitoria'
+    elif num == 1571:
+        return 'marcilio_dias'
+    elif num == 1744:
+        return 'canudos'
+    elif num == 1574:
+        return 'joaquim_nabuco'
+    elif num == 1648:
+        return 'sapucaia'
+
+def type_lens(num):
+    if num == 1:
+        return 'VS'
+    elif num == 2:
+        return 'Multifocal'
--- system/__init__.py
+++ system/__init__.py
@@ -1,61 +1,56 @@
-from datetime import date
 import functions
-import _sqlite3
-
+from datetime import date
+import mysql.connector
 
 print('\033[33mControle de serviços Óticas Carol\033[m')
-print('-'*33)
-
-date = date.today()
-temporary_comparison = ''
-
-# for recording the date
-
+print('-' * 33)
 
 #  date entry for service
-option = functions.inteiro('(1) Para cadastrar / (2) Para pesquisar: ')
-if option == 1:
-    while True:
-        store = functions.check_store('Digite o número da filial: ')
-        os = functions.inteiro('Digite o número da OS: ')
-        service_type = functions.inteiro('Digite 1 para "VS" ou 2 para "Multifocal": ')
-        tem_store = service_type
-        if service_type == 1:
-            cont_vs = 1
-            service_type = 'VS'
-            file_txt = open('arquivo.txt', 'at')  # file open for recording
-            temporary = str(f'Loja: {store} | Seq: {os} | Tipo: {service_type}\n')
-            read_file = open('arquivo.txt', 'r')
-            for line in read_file:
-                if temporary != line:
-                    temporary_comparison = temporary
+while True:
+    #  current date
+    date = date.today()
+    option = functions.inteiro('(1) Para cadastrar / (2) Para pesquisar: ')
+    if option == 1:
+        while True:
+            store = functions.check_store('Digite o número da filial: ')
+            os = functions.integer_with_bar('Digite o número da OS: ')
+            service_type = functions.inteiro('Digite 1 para "VS" ou 2 para "Multifocal": ')
+            store_comparation = functions.comparation(store)
+            lens = functions.type_lens(service_type)
+            if service_type == 1:
+                try:
+                    connection = mysql.connector.connect(host='localhost', user='root', password='',
+                                                         database='controle_de_serviços')
+                    cursor = connection.cursor()
+                    cursor.execute(f"INSERT INTO {store_comparation} (dia, sequencia, tipo) "
+                                   f"VALUES ('{date}', '{os}', '{lens}')")
+                    connection.commit()
+                    connection.close()
+                except:
+                    print(f'\033[31mNão foi possível acessar o banco de dados!\033[m')
                 else:
-                    print(f'\033[31mVocê terá que alterar o código da filial ou o número da OS!\033[m')
-                    store = functions.check_store('Digite o código da filial: ')
-                    os = functions.inteiro('Digite o número da OS: ')
-                    correct = str(f'Loja: {store} | Seq: {os} | Tipo: {service_type}\n')
-                    temporary = correct
-                    temporary_comparison = temporary
-            file_txt.write(temporary_comparison)
-            file_txt.close()  # closed file for recording
-        elif service_type == 2:
-            cont_multi = 1
-            service_type = 'Multifocal'
-            file_txt = open('arquivo.txt', 'at')  # file open for recording
-            temporary = str(f'Loja: {store} | Seq: {os} | Tipo: {service_type}\n')
-            read_file = open('arquivo.txt', 'r')
-            for line in read_file:
-                if temporary != line:
-                    file_txt.write(temporary)
+                    print(f'\033[33mSalvo com sucesso no banco de dados!\033[m')
+            elif service_type == 2:
+                try:
+                    connection = mysql.connector.connect(host='localhost', user='root', password='',
+                                                         database='controle_de_serviços')
+                    cursor = connection.cursor()
+                    cursor.execute(f"INSERT INTO {store_comparation} (dia, sequencia, tipo) "
+                                   f"VALUES ('{date}', '{os}', '{lens}')")
+                    connection.commit()
+                    connection.close()
+                except:
+                    print(f'\033[31mNão foi possível acessar o banco de dados!\033[m')
                 else:
-                    print(f'\033[31mVocê terá que alterar o código da filial ou o número da OS!\033[m')
-                    store = functions.check_store('Digite o códgio da filial: ')
-                    os = functions.inteiro('Digite o número da OS: ')
-                    correct = str(f'Loja: {store} | Seq: {os} | Tipo: {service_type}\n')
-                    file_txt.write(correct)
-            file_txt.close()  # closed file for recording
-        else:
-            print('\033[31mOpção inválida!\033[m')
-            continue
-else:
-    print('2')
+                    print(f'\033[33mSalvo com sucesso no banco de dados!\033[m')
+            else:
+                print('\033[31mOpção inválida!\033[m')
+                continue
+            break
+        continue
+    elif option == 2:
+        print('2')
+        continue
+    else:
+        print(f'\033[31mOpção Inválida no primeiro passo!\033[m')
+        continue
